3176afebb9ebb0cc7617a909ab822a81a642efb4
aUsefulMethod <belllabenthusiast@gmail.com>
2022-04-20 13:29:46 -0500

    small-fixes

--- navigation.php
+++ navigation.php
@@ -1,16 +1,5 @@
 <?php
-echo '    <nav class="navbar navbar-inverse">';
-echo '        <div class="container-fluid">';
-echo '          <div class="navbar-header">';
-echo '            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">';
-echo '              <span class="icon-bar"></span>';
-echo '              <span class="icon-bar"></span>';
-echo '              <span class="icon-bar"></span>';
-echo '            </button>';
-echo '            <a class="navbar-brand" href="#">Travis Sauer\'s Resume Website</a>';
-echo '          </div>';
-echo '          <div class="collapse navbar-collapse" id="myNavbar">';
-echo '            <ul class="nav navbar-nav">';
+echo '<ul class="nav nav-tabs">';
 if(isset($_GET['page']) && $_GET['page'] != ""){
     switch($_GET['page']){
         case "school":
@@ -39,8 +28,5 @@ if(isset($_GET['page']) && $_GET['page'] != ""){
             
     }
 }
-echo '            </ul>';
-echo '          </div>';
-echo '        </div>';
-echo '      </nav>';
+echo '</ul>';
 ?>
