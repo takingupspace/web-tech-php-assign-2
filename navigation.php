a366109a950e65c9654101b21f856f2d19f7176c
aUsefulMethod <belllabenthusiast@gmail.com>
2022-04-20 13:31:19 -0500

    small-fixes

--- navigation.php
+++ navigation.php
@@ -7,24 +7,28 @@ if(isset($_GET['page']) && $_GET['page'] != ""){
             echo '              <li><a href="index.php?page=work">Work</a></li>';
             echo '              <li><a href="index.php?page=school">School</a></li>';
             echo '              <li><a href="index.php?page=contact">Contact</a></li>';
+            break;
         
         case "work":
             echo '<li class="active"><a href="index.php?page=home">Home</a></li>';
             echo '              <li><a href="index.php?page=work">Work</a></li>';
             echo '              <li><a href="index.php?page=school">School</a></li>';
             echo '              <li><a href="index.php?page=contact">Contact</a></li>';
+            break;
         
         case "contact":
             echo '<li class="active"><a href="index.php?page=home">Home</a></li>';
             echo '              <li><a href="index.php?page=work">Work</a></li>';
             echo '              <li><a href="index.php?page=school">School</a></li>';
             echo '              <li><a href="index.php?page=contact">Contact</a></li>';
+            break;
 
         default:
             echo '<li class="active"><a href="index.php?page=home">Home</a></li>';
             echo '              <li><a href="index.php?page=work">Work</a></li>';
             echo '              <li><a href="index.php?page=school">School</a></li>';
             echo '              <li><a href="index.php?page=contact">Contact</a></li>';
+            break;
             
     }
 }
